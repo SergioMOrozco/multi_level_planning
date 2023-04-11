7b656cec46aedb6eae847c0f9d2c78fbf4ebfb6e
Mr-vedant-gupta <vedant_gupta@brown.edu>
2023-04-11 14:23:00 -0400

    fixed planner to return plan of least length + least number of gaps

--- planner.py
+++ planner.py
@@ -3,6 +3,7 @@ import queue
 import numpy as np
 from main import mdp_2, mdp_1, mdp_0
 from neighbourhood import Neighbourhood
+import math
 
 
 class Planner():
@@ -12,6 +13,21 @@ class Planner():
         # arr1 = np.zeros((8, 8))
         # arr1[1, 0] = 1
         # print(N(arr1))
+
+    def extract_plan_efficiency(self, state, parents): # returns [plan length, number of gaps stitched]
+        # state:[previous_state, option used, state from which option executed]
+        plan_len = 0
+        num_gaps = 0
+        state = self.to_tuple(state)
+        if state not in parents:
+            return [math.inf, math.inf]
+        while parents[state] != None:
+            prev_option_term_state, option, option_start_state = parents[state]
+            plan_len += 1
+            if not np.array_equal(prev_option_term_state, option_start_state):
+                num_gaps += 1
+            state = self.to_tuple(prev_option_term_state)
+        return [plan_len, num_gaps]
 
 
     def extract_plan(self, state, parents): #backtrack using parents and return the plan
@@ -55,22 +71,44 @@ class Planner():
         if a_subset_b(S, self.N(G)):
             return True, []
         reached = [self.to_tuple(S)]
-        frontier = queue.Queue() 
-        
+        frontier1 = queue.Queue() 
+        frontier2 = queue.Queue()
+        goal_reached = False
+        cur_efficiency = [math.inf, math.inf]
+        term_state = None
         parents = {self.to_tuple(S):None} #parents format -> state:[previous_state, option used, state from which option executed]
-        frontier.put(S)
-        while (not frontier.empty()):
-            state = frontier.get()
+        frontier1.put(S)
+        while (True):
+            if frontier1.empty():
+                if goal_reached:
+                    return True, self.extract_plan(term_state, parents)
+                elif frontier2.empty():
+                    break
+                else:
+                    frontier1 = frontier2
+                    frontier2 = queue.Queue()
+            state = frontier1.get()
             for i in self.get_sucessors(state):
                 state_to_tup = self.to_tuple(i[2])
                 if a_subset_b(i[2], self.N(G)):
+                    goal_reached = True
+                    if self.to_tuple(i[2]) in parents:
+                        old_parent  = parents[self.to_tuple(i[2])]
+                    else:
+                        old_parent = None
                     parents[self.to_tuple(i[2])] = [state, i[0], i[1]]
-                    return True, self.extract_plan(i[2], parents)
+                    new_efficiency = self.extract_plan_efficiency(i[2], parents)
+                    if new_efficiency < cur_efficiency:
+                        term_state = i[2]
+                        cur_efficiency = new_efficiency
+                    else:
+                        parents[self.to_tuple(i[2])] = old_parent
+                    
                     
                 elif state_to_tup not in reached:
                     reached.append(state_to_tup)
                     parents[self.to_tuple(i[2])] = [state, i[0], i[1]]
-                    frontier.put(i[2])
+                    frontier2.put(i[2])
         return False, []
 
 if __name__ == "__main__":
